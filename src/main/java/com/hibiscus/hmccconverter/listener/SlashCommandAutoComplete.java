ed5f20221c453a980d52a415f638e1ff19c91dad
LoJoSho <LoJoShogaming@gmail.com>
2025-02-16 12:27:53 -0600

    feat: make lists of words final

--- src/main/java/com/hibiscus/hmccconverter/listener/SlashCommandAutoComplete.java
+++ src/main/java/com/hibiscus/hmccconverter/listener/SlashCommandAutoComplete.java
@@ -10,12 +10,12 @@ import java.util.stream.Stream;
 
 public class SlashCommandAutoComplete extends ListenerAdapter {
 
-    private String[] words = new String[]{
+    private final String[] words = new String[]{
             "MCCosmetics",
             "CosmeticCore",
             "MagicCosmetics"
     };
-    private String[] slots = new String[]{
+    private final String[] slots = new String[]{
             "HELMET",
             "CHESTPLATE",
             "LEGGINGS",
@@ -29,14 +29,19 @@ public class SlashCommandAutoComplete extends ListenerAdapter {
 
     @Override
     public void onCommandAutoCompleteInteraction(CommandAutoCompleteInteractionEvent event) {
-        if (event.getName().equals("convert") && event.getFocusedOption().getName().equals("originalfiletype")) {
+        System.out.println(event.getName());
+        if (!event.getName().equalsIgnoreCase("convert")) {
+            return;
+        }
+
+        if (event.getFocusedOption().getName().equals("originalfiletype")) {
             List<Command.Choice> options = Stream.of(words)
                     .filter(word -> word.startsWith(event.getFocusedOption().getValue())) // only display words that start with the user's current input
                     .map(word -> new Command.Choice(word, word)) // map the words to choices
                     .collect(Collectors.toList());
             event.replyChoices(options).queue();
         }
-        if (event.getName().equals("convert") && event.getFocusedOption().getName().equals("forceslot")) {
+        if (event.getFocusedOption().getName().equals("forceslot")) {
             List<Command.Choice> options = Stream.of(slots)
                     .filter(word -> word.startsWith(event.getFocusedOption().getValue())) // only display words that start with the user's current input
                     .map(word -> new Command.Choice(word, word)) // map the words to choices
